b504e659d2ada1cfc957e200a90267b49ff178b2
HS831 <harrymishra831@gmail.com>
2022-06-01 18:35:16 +0530

    Updated code v1.0.2

--- utils/email.js
+++ utils/email.js
@@ -11,7 +11,7 @@ const sendEmail = async options => {
   });
 
   const mailOptions = {
-    from: 'Hacked!!! <youraccount@hacked.com>',
+    from: 'Harshit Mishra <harrymishra831@gmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message
